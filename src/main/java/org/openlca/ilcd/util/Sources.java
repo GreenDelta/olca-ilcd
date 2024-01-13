87bb4799d944a525dc62f4ce4c2572fe06d44420
msrocka <michael.srocka@gmail.com>
2024-01-13 18:01:00 +0100

    remove SourceBag

--- src/main/java/org/openlca/ilcd/util/Sources.java
+++ src/main/java/org/openlca/ilcd/util/Sources.java
@@ -1,9 +1,5 @@
 package org.openlca.ilcd.util;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.List;
 import org.openlca.ilcd.commons.DataEntry;
 import org.openlca.ilcd.commons.Publication;
 import org.openlca.ilcd.sources.AdminInfo;
@@ -11,8 +7,12 @@ import org.openlca.ilcd.sources.DataSetInfo;
 import org.openlca.ilcd.sources.FileRef;
 import org.openlca.ilcd.sources.Source;
 import org.openlca.ilcd.sources.SourceInfo;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
 public final class Sources {
 
@@ -20,77 +20,82 @@ public final class Sources {
 	}
 
 	public static SourceInfo getSourceInfo(Source s) {
-		if (s == null)
-			return null;
-		return s.sourceInfo;
+		return s != null
+			? s.sourceInfo
+			: null;
 	}
 
-	public static SourceInfo sourceInfo(Source s) {
-		if (s.sourceInfo == null)
+	public static SourceInfo forceSourceInfo(Source s) {
+		if (s.sourceInfo == null) {
 			s.sourceInfo = new SourceInfo();
+		}
 		return s.sourceInfo;
 	}
 
 	public static DataSetInfo getDataSetInfo(Source s) {
-		SourceInfo si = getSourceInfo(s);
-		if (si == null)
-			return null;
-		return si.dataSetInfo;
+		var info = getSourceInfo(s);
+		return info != null
+			? info.dataSetInfo
+			: null;
 	}
 
-	public static DataSetInfo dataSetInfo(Source s) {
-		SourceInfo si = sourceInfo(s);
-		if (si.dataSetInfo == null)
-			si.dataSetInfo = new DataSetInfo();
-		return si.dataSetInfo;
+	public static DataSetInfo forceDataSetInfo(Source s) {
+		var info = forceSourceInfo(s);
+		if (info.dataSetInfo == null) {
+			info.dataSetInfo = new DataSetInfo();
+		}
+		return info.dataSetInfo;
 	}
 
 	public static AdminInfo getAdminInfo(Source s) {
-		if (s == null)
-			return null;
-		return s.adminInfo;
+		return s != null
+			? s.adminInfo
+			: null;
 	}
 
-	public static AdminInfo adminInfo(Source s) {
-		if (s.adminInfo == null)
+	public static AdminInfo forceAdminInfo(Source s) {
+		if (s.adminInfo == null) {
 			s.adminInfo = new AdminInfo();
+		}
 		return s.adminInfo;
 	}
 
 	public static DataEntry getDataEntry(Source s) {
-		AdminInfo ai = getAdminInfo(s);
-		if (ai == null)
-			return null;
-		return ai.dataEntry;
+		var info = getAdminInfo(s);
+		return info != null
+			? info.dataEntry
+			: null;
 	}
 
-	public static DataEntry dataEntry(Source s) {
-		AdminInfo ai = adminInfo(s);
-		if (ai.dataEntry == null)
-			ai.dataEntry = new DataEntry();
-		return ai.dataEntry;
+	public static DataEntry forceDataEntry(Source s) {
+		var info = forceAdminInfo(s);
+		if (info.dataEntry == null) {
+			info.dataEntry = new DataEntry();
+		}
+		return info.dataEntry;
 	}
 
-	public static List<FileRef> getFileRefs(Source source) {
-		if (source == null || source.sourceInfo == null)
+	public static List<FileRef> getFileRefs(Source s) {
+		if (s == null || s.sourceInfo == null)
 			return Collections.emptyList();
-		if (source.sourceInfo.dataSetInfo == null)
-			return Collections.emptyList();
-		return source.sourceInfo.dataSetInfo.files;
+		return s.sourceInfo.dataSetInfo == null
+			? Collections.emptyList()
+			: s.sourceInfo.dataSetInfo.files;
 	}
 
 	public static Publication getPublication(Source s) {
-		AdminInfo ai = getAdminInfo(s);
-		if (ai == null)
-			return null;
-		return ai.publication;
+		var info = getAdminInfo(s);
+		return info != null
+			? info.publication
+			: null;
 	}
 
-	public static Publication publication(Source s) {
-		AdminInfo ai = adminInfo(s);
-		if (ai.publication == null)
-			ai.publication = new Publication();
-		return ai.publication;
+	public static Publication forcePublication(Source s) {
+		var info = forceAdminInfo(s);
+		if (info.publication == null) {
+			info.publication = new Publication();
+		}
+		return info.publication;
 	}
 
 	/**
@@ -110,7 +115,7 @@ public final class Sources {
 			}
 			return URLDecoder.decode(s, StandardCharsets.UTF_8);
 		} catch (Exception e) {
-			Logger log = LoggerFactory.getLogger(Sources.class);
+			var log = LoggerFactory.getLogger(Sources.class);
 			log.error("could not get file name from " + ref.uri, e);
 			return null;
 		}
---
+++ src/test/java/org/openlca/ilcd/io/SourceTest.java
@@ -0,0 +1,54 @@
+package org.openlca.ilcd.io;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openlca.ilcd.commons.LangString;
+import org.openlca.ilcd.sources.DataSetInfo;
+import org.openlca.ilcd.sources.Source;
+import org.openlca.ilcd.util.Categories;
+import org.openlca.ilcd.util.Sources;
+
+import static org.junit.Assert.*;
+
+public class SourceTest {
+
+	private Source source;
+	private DataSetInfo info;
+
+	@Before
+	public void setUp() throws Exception {
+		source = Tests.read(Source.class, "source.xml");
+		info = Sources.getDataSetInfo(source);
+		assertNotNull(info);
+	}
+
+	@Test
+	public void testUUID() {
+		assertEquals("2c699413-f88b-4cb5-a56d-98cb4068472f", source.getUUID());
+	}
+
+	@Test
+	public void testName() {
+		assertEquals("IMA-Europe_Plastic_Clay_diagramme_"
+				+ "2c699413-f88b-4cb5-a56d-98cb4068472f.jpg",
+			LangString.getFirst(info.name));
+	}
+
+	@Test
+	public void testDescription() {
+		assertNull(LangString.getFirst(info.description));
+	}
+
+	@Test
+	public void testCitation() {
+		assertEquals("GaBi database", info.citation);
+	}
+
+	@Test
+	public void testCategoryPath() {
+		String[] path = Categories.getPath(source);
+		assertEquals(path.length, 1);
+		assertEquals("Images", path[0]);
+	}
+
+}
