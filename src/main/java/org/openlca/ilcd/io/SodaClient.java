a6c0ee912944282b2b5aac8f628e3f478343d5d9
msrocka <michael.srocka@gmail.com>
2024-05-10 17:09:44 +0200

    soda: better query handling

--- src/main/java/org/openlca/ilcd/io/SodaClient.java
+++ src/main/java/org/openlca/ilcd/io/SodaClient.java
@@ -1,15 +1,13 @@
 package org.openlca.ilcd.io;
 
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.Invocation;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.Cookie;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
-import jakarta.ws.rs.core.Response.Status.Family;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
@@ -26,13 +24,16 @@ import org.openlca.ilcd.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Cookie;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import jakarta.ws.rs.core.Response.Status.Family;
 
 
 /**
@@ -69,8 +70,7 @@ public class SodaClient implements DataStore {
 	public SodaClient login(String user, String password) {
 		log.info("Authenticate user: {}", user);
 		var response = client.target(url)
-			.path("authenticate")
-			.path("login")
+			.path("authenticate/login")
 			.queryParam("userName", user)
 			.queryParam("password", password)
 			.request()
@@ -86,38 +86,32 @@ public class SodaClient implements DataStore {
 	}
 
 	public AuthInfo getAuthInfo() {
-		log.trace("Get authentication information.");
-		var r = resource("authenticate", "status");
-		var response = cookies(r).get();
-		eval(response);
-		return response.readEntity(AuthInfo.class);
+		log.trace("get authentication status: /authenticate/status");
+		var resp = new Req().p("authenticate/status").get();
+		eval(resp);
+		return resp.readEntity(AuthInfo.class);
 	}
 
 	public DataStockList getDataStockList() {
 		log.trace("get data stock list: /datastocks");
-		var r = resource("datastocks");
-		return cookies(r).get(DataStockList.class);
+		return new Req().p("datastocks").get(DataStockList.class);
 	}
 
 	public CategorySystemList getCategorySystemList() {
 		log.trace("get category system list: /categorySystems");
-		var r = resource("categorySystems");
-		return cookies(r).get(CategorySystemList.class);
+		return new Req().p("categorySystems").get(CategorySystemList.class);
 	}
 
 	public CategorySystem getCategorySystem(String name) {
 		log.trace("get category system list: /categorySystems/{}", name);
-		var r = resource("categorySystems", name);
-		return cookies(r).get(CategorySystem.class);
+		return new Req().p("categorySystems").p(name).get(CategorySystem.class);
 	}
 
 	@Override
 	public <T extends IDataSet> T get(Class<T> type, String id) {
-		var r = resource(Dir.get(type), id).queryParam("format", "xml");
-		log.info("Get resource: {}", r.getUri());
-		var response = cookies(r).get();
-		eval(response);
-		try (var stream = response.readEntity(InputStream.class)) {
+		var resp = new Req().onStock().p(type).p(id).q("format", "xml").get();
+		eval(resp);
+		try (var stream = resp.readEntity(InputStream.class)) {
 			return Xml.read(type, stream);
 		} catch (Exception e) {
 			throw new RuntimeException("Failed to load resource " + id
@@ -132,18 +126,16 @@ public class SodaClient implements DataStore {
 			log.error("failed to read UUID from dataset {}", ds);
 			return;
 		}
-		var r = resource(Dir.get(ds.getClass()));
-		log.info("Publish resource: {}/{}", r.getUri(), uid);
+		var req = new Req().p(ds.getClass())
+			.build()
+			.accept(MediaType.APPLICATION_XML);
 		try {
 			byte[] bytes = Xml.toBytes(ds);
-			var builder = cookies(r).accept(MediaType.APPLICATION_XML);
 			if (Strings.notEmpty(dataStockId)) {
-				log.trace("post to data stock {}", dataStockId);
-				builder = builder.header("stock", dataStockId);
+				req = req.header("stock", dataStockId);
 			}
-			var response = builder.post(Entity.xml(bytes));
+			var response = req.post(Entity.xml(bytes));
 			eval(response);
-			log.trace("Server response: {}", fetchMessage(response));
 		} catch (Exception e) {
 			throw new RuntimeException("Failed to upload data set + " + ds, e);
 		}
@@ -167,12 +159,23 @@ public class SodaClient implements DataStore {
 			formData.bodyPart(xmlPart);
 
 			// add the other files
-			addFiles(files, formData);
+			if (files != null) {
+				for (File file : files) {
+					if (file == null)
+						continue;
+					var is = new FileInputStream(file);
+					var part = new FormDataBodyPart(file.getName(),
+						is, MediaType.MULTIPART_FORM_DATA_TYPE);
+					formData.bodyPart(part);
+				}
+			}
 
-
-			var r = resource("sources/withBinaries");
-			var resp = cookies(r)
-				.post(Entity.entity(formData, MediaType.MULTIPART_FORM_DATA_TYPE));
+			var req = new Req().p("sources/withBinaries").build();
+			if (Strings.notEmpty(dataStockId)) {
+				req = req.header("stock", dataStockId);
+			}
+			var resp = req.post(Entity.entity(
+				formData, MediaType.MULTIPART_FORM_DATA_TYPE));
 			eval(resp);
 			log.trace("Server response: {}", fetchMessage(resp));
 		} catch (Exception e) {
@@ -180,29 +183,14 @@ public class SodaClient implements DataStore {
 		}
 	}
 
-	private void addFiles(File[] files, FormDataMultiPart multiPart)
-		throws Exception {
-		if (files == null)
-			return;
-		for (File file : files) {
-			if (file == null)
-				continue;
-			var is = new FileInputStream(file);
-			var part = new FormDataBodyPart(file.getName(),
-				is, MediaType.MULTIPART_FORM_DATA_TYPE);
-			multiPart.bodyPart(part);
-		}
-	}
-
 	@Override
 	public InputStream getExternalDocument(String sourceId, String fileName) {
-		var r = resource("sources", sourceId, fileName);
-		log.info("Get external document {} for source {}", fileName, sourceId);
-		var response = cookies(r)
+		var r = new Req().onStock().p("sources").p(sourceId).p(fileName)
+			.build()
 			.accept(MediaType.APPLICATION_OCTET_STREAM)
 			.get();
-		eval(response);
-		return response.readEntity(InputStream.class);
+		eval(r);
+		return r.readEntity(InputStream.class);
 	}
 
 	@Override
@@ -219,12 +207,10 @@ public class SodaClient implements DataStore {
 
 	@Override
 	public <T extends IDataSet> boolean contains(Class<T> type, String id) {
-		var r = resource(Dir.get(type), id)
-			.queryParam("format", "xml");
-		log.trace("Contains resource {} ?", r.getUri());
-		var response = cookies(r).head();
-		log.trace("Server response: {}", response);
-		return response.getStatus() == Status.OK.getStatusCode();
+		var req = new Req().onStock().p(type).p(id).q("format", "xml");
+		try (var resp = req.build().head()) {
+			return resp.getStatus() == Status.OK.getStatusCode();
+		}
 	}
 
 	/**
@@ -233,51 +219,41 @@ public class SodaClient implements DataStore {
 	public boolean contains(Ref ref) {
 		if (ref == null || ref.getType() == null || ref.getUUID() == null)
 			return false;
-		var r = resource(Dir.get(ref.getDataSetClass()), ref.getUUID())
-			.queryParam("format", "xml");
-		if (ref.getVersion() != null)
-			r = r.queryParam("version", ref.getVersion());
-		try (var response = cookies(r).head()) {
-			return response.getStatus() == Status.OK.getStatusCode();
+		var req = new Req().onStock()
+			.p(ref.getDataSetClass())
+			.p(ref.getUUID())
+			.q("format", "xml");
+		if (ref.getVersion() != null) {
+			req.q("version", ref.getVersion());
+		}
+		try (var resp = req.build().head()) {
+			return resp.getStatus() == Status.OK.getStatusCode();
 		}
 	}
 
-	public DescriptorList search(Class<?> type, String name) {
-		try {
-			String term = name == null ? "" : name.trim();
-			var r = Strings.nullOrEmpty(dataStockId)
-				? resource(Dir.get(type))
-				: resource("datastocks", dataStockId, Dir.get(type));
-			r = r.queryParam("search", "true")
-				.queryParam("name", term);
-			log.trace("Search resources: {}", r.getUri());
-			return cookies(r).get(DescriptorList.class);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+	public <T extends IDataSet> DescriptorList search(Class<T> type, String name) {
+		var q = new SodaQuery()
+			.withSearch(true)
+			.withName(name == null ? "" : name.trim());
+		return query(type, q);
 	}
 
-	public List<Descriptor<?>> getDescriptors(Class<?> type) {
-		log.debug("get descriptors for {}", type);
+	public <T extends IDataSet> DescriptorList query(Class<T> type, SodaQuery q) {
+		if (q == null) {
+			return new DescriptorList();
+		}
+		return new Req().onStock().p(type).q(q).get(DescriptorList.class);
+	}
+
+	public <T extends IDataSet> List<Descriptor<?>> getDescriptors(Class<T> type) {
 		try {
-			var r = Strings.nullOrEmpty(dataStockId)
-				? resource(Dir.get(type))
-				: resource("datastocks", dataStockId, Dir.get(type));
-			r = r.queryParam("pageSize", "1000");
+			var q = new SodaQuery().withPageSize(1000);
 			var list = new ArrayList<Descriptor<?>>();
-			int total;
-			int idx = 0;
 			do {
-				log.debug("get descriptors for {} @startIndex={}", type, idx);
-				r = r.queryParam("startIndex", Integer.toString(idx));
-				DescriptorList data = cookies(r).get(DescriptorList.class);
-				total = data.getTotalSize();
-				int fetched = data.getDescriptors().size();
-				if (fetched == 0)
-					break;
-				list.addAll(data.getDescriptors());
-				idx += fetched;
-			} while (list.size() < total);
+				var page = new Req().onStock().p(type).q(q).get(DescriptorList.class);
+				list.addAll(page.getDescriptors());
+				q = q.next(page).orElse(null);
+			} while (q != null);
 			return list;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
@@ -302,26 +278,9 @@ public class SodaClient implements DataStore {
 				"data-stock " + idOrName + " does not exist on server");
 		}
 
-		var r = resource("datastocks", id, "export");
-		var response = cookies(r).get();
-		eval(response);
-		return response.readEntity(InputStream.class);
-	}
-
-	private WebTarget resource(String... path) {
-		var target = client.target(url);
-		for (String p : path) {
-			target = target.path(p);
-		}
-		return target;
-	}
-
-	private Invocation.Builder cookies(WebTarget target) {
-		var builder = target.request();
-		for (Cookie c : cookies) {
-			builder.cookie(c);
-		}
-		return builder;
+		var resp = new Req().p("datastocks").p( id).p( "export").get();
+		eval(resp);
+		return resp.readEntity(InputStream.class);
 	}
 
 	private void eval(Response resp) {
@@ -344,5 +303,65 @@ public class SodaClient implements DataStore {
 	@Override
 	public void close() {
 		client.close();
+	}
+
+	private class Req {
+
+		private WebTarget target;
+
+		Req() {
+			target = client.target(url);
+		}
+
+		Req onStock() {
+			if (dataStockId != null) {
+				p("datastocks").p(dataStockId);
+			}
+			return this;
+		}
+
+		/**
+		 * Adds a path segment to the request.
+		 */
+		Req p(String path) {
+			target = target.path(path);
+			return this;
+		}
+
+		<T extends IDataSet> Req p(Class<T> type) {
+			return p(Dir.get(type));
+		}
+
+		/**
+		 * Adds a query parameter to the request
+		 */
+		Req q(String param, String value) {
+			target = target.queryParam(param, value);
+			return this;
+		}
+
+		Req q(SodaQuery q) {
+			if (q != null) {
+				target = q.applyOn(target);
+			}
+			return this;
+		}
+
+		Invocation.Builder build() {
+			log.trace("create request: {}", target.getUri());
+			var builder = target.request();
+			for (var c : cookies) {
+				builder.cookie(c);
+			}
+			return builder;
+		}
+
+		Response get() {
+			return build().get();
+		}
+
+		<T> T get(Class<T> type) {
+			return build().get(type);
+		}
 	}
 }
---
+++ src/main/java/org/openlca/ilcd/io/SodaQuery.java
@@ -0,0 +1,153 @@
+package org.openlca.ilcd.io;
+
+import java.util.Optional;
+
+import org.openlca.ilcd.commons.Copyable;
+import org.openlca.ilcd.descriptors.DescriptorList;
+
+import jakarta.ws.rs.client.WebTarget;
+
+/**
+ * A query for searching for datasets or iterating over the available datasets
+ * of a Soda instance. See the official documentation for details about what
+ * these query parameters mean.
+ */
+public class SodaQuery implements Copyable<SodaQuery> {
+
+	private int startIndex = 0;
+	private int pageSize = 500;
+
+	private Boolean search;
+	private Boolean distributed;
+	private String name;
+	private String description;
+	private String classId;
+	private String lang;
+	private Boolean langFallback;
+	private Boolean allVersions;
+	private Boolean countOnly;
+
+	public SodaQuery withStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+		return this;
+	}
+
+	public SodaQuery withPageSize(int pageSize) {
+		this.pageSize = pageSize;
+		return this;
+	}
+
+	public SodaQuery withSearch(Boolean search) {
+		this.search = search;
+		return this;
+	}
+
+	public SodaQuery withDistributed(Boolean distributed) {
+		this.distributed = distributed;
+		return this;
+	}
+
+	public SodaQuery withName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public SodaQuery withDescription(String description) {
+		this.description = description;
+		return this;
+	}
+
+	public SodaQuery withClassId(String classId) {
+		this.classId = classId;
+		return this;
+	}
+
+	public SodaQuery withLang(String lang) {
+		this.lang = lang;
+		return this;
+	}
+
+	public SodaQuery withLangFallback(Boolean langFallback) {
+		this.langFallback = langFallback;
+		return this;
+	}
+
+	public SodaQuery withAllVersions(Boolean allVersions) {
+		this.allVersions = allVersions;
+		return this;
+	}
+
+	public SodaQuery withCountOnly(Boolean countOnly) {
+		this.countOnly = countOnly;
+		return this;
+	}
+
+	WebTarget applyOn(WebTarget r) {
+		if (r == null)
+			return r;
+		var t = r.queryParam("pageSize", pageSize)
+			.queryParam("startIndex", startIndex);
+		if (search != null) {
+			t = r.queryParam("search", search);
+		}
+		if (distributed != null) {
+			t = r.queryParam("distributed", distributed);
+		}
+		if (name != null) {
+			t = r.queryParam("name", name);
+		}
+		if (description != null) {
+			t = r.queryParam("description", description);
+		}
+		if (classId != null) {
+			t = r.queryParam("classId", classId);
+		}
+		if (lang != null) {
+			t = r.queryParam("lang", lang);
+		}
+		if (langFallback != null) {
+			t = r.queryParam("langFallback", langFallback);
+		}
+		if (allVersions != null) {
+			t = r.queryParam("allVersions", allVersions);
+		}
+		if (countOnly != null) {
+			t = r.queryParam("countOnly", countOnly);
+		}
+		return t;
+	}
+
+	/**
+	 * Returns a query for the next page related to the given result of this
+	 * query. The given result has to be retrieved from this query, otherwise
+	 * a return next query may not work correctly.
+	 */
+	public Optional<SodaQuery> next(DescriptorList res) {
+		if (res == null
+			|| res.getStartIndex() != startIndex
+			|| res.getPageSize() != pageSize)
+			return Optional.empty();
+		int nextIndex = startIndex + pageSize;
+		int total = res.getTotalSize();
+		if (nextIndex >= total)
+			return Optional.empty();
+		var q = this.copy().withStartIndex(nextIndex);
+		return Optional.of(q);
+	}
+
+	@Override
+	public SodaQuery copy() {
+		return new SodaQuery()
+			.withStartIndex(startIndex)
+			.withPageSize(pageSize)
+			.withSearch(search)
+			.withDistributed(distributed)
+			.withName(name)
+			.withDescription(description)
+			.withClassId(classId)
+			.withLang(lang)
+			.withLangFallback(langFallback)
+			.withAllVersions(allVersions)
+			.withCountOnly(countOnly);
+	}
+}
